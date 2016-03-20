32793d2eef8a1568c744b7cdc1b9bf2eee18cd87
Zhigang Fang <zhigang1992@gmail.com>
2016-03-20 11:52:06 +0800

    Restructure

--- Source/On.swift
+++ Source/On.swift
@@ -13,19 +13,19 @@ public protocol Onable {
     
 }
 
-class DummyTarget: NSObject {
+private class DummyTarget: NSObject {
     let action:UIControlEvents -> Void
     init(action:UIControlEvents -> Void) {
         self.action = action
     }
-    func execute(event:UIControlEvents) {
+    @objc func execute(event:UIControlEvents) {
         self.action(event)
     }
 }
 
 private var callbackStoreKey:UInt8 = 0
 
-extension UIControl {
+private extension UIControl {
     var callbacks:[UInt: DummyTarget] {
         get {
             return objc_getAssociatedObject(self, &callbackStoreKey) as? [UInt: DummyTarget] ?? [:]
@@ -39,14 +39,14 @@ extension UIControl {
 extension UIControl : Onable {}
 
 extension Onable where Self : UIControl {
-    func on(event:UIControlEvents, action:(Self, UIControlEvents) -> Void) {
+    public func on(event:UIControlEvents, action:(Self, UIControlEvents) -> Void) {
         let dummyTarget = DummyTarget(action: {
             action(self, $0)
         })
         self.addTarget(dummyTarget, action: #selector(DummyTarget.execute(_:)), forControlEvents: event)
         self.callbacks[event.rawValue] = dummyTarget
     }
-    func off(event:UIControlEvents) {
+    public func off(event:UIControlEvents) {
         guard let dummyTarget = self.callbacks[event.rawValue] else { return }
         self.removeTarget(dummyTarget, action: #selector(DummyTarget.execute(_:)), forControlEvents: event)
         self.callbacks[event.rawValue] = nil
